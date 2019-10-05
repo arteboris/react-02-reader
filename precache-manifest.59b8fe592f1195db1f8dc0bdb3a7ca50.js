f7c071ff55aae765d220b4fc5075248729a02c40
arteboris <arteboris@ukr.net>
2019-10-05 12:10:20 +0300

    Updates

---
+++ precache-manifest.59b8fe592f1195db1f8dc0bdb3a7ca50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a60099f84b3dfa3b322a8000b004100",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "e0468cfa9f0b04d5d52e",
+    "url": "/goit-react-hw-02-reader/static/css/main.7a806c28.chunk.css"
+  },
+  {
+    "revision": "6e2d8c189ca8ccfa8fce",
+    "url": "/goit-react-hw-02-reader/static/js/2.58322872.chunk.js"
+  },
+  {
+    "revision": "e0468cfa9f0b04d5d52e",
+    "url": "/goit-react-hw-02-reader/static/js/main.6a18a950.chunk.js"
+  },
+  {
+    "revision": "c0f2b9a1bd18860287f3",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.0eb8eb5f.js"
+  }
+]);
